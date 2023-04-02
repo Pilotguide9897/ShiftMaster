fef4fe63914fd68806966494dacc6306099a3581
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-04-01 21:12:16 -0300

    updates

--- Develop/index.js
+++ Develop/index.js
@@ -4,27 +4,93 @@ const inquirerPrompts = require('./Prompts/inquirerprompts');
 const queries = require ('./queries');
 const dbConnection = require('./db');
 
-const mainMenuHandler = function () {
+const mainMenuHandler = async function () {
     let response = await inquirer.prompt(inquirerPrompts.userAction);
 
-
-switch (action) {
-    case
-        return ;
-    case
-        return ;
-    case
-        return ;
-    case
-        return ;
-    case
-        return ;
-    case
-        return ;
+switch (action) { // I can add the other bonus objectives here too.
+    case 'View All Departments':
+        return viewAllDepartments(queries.);
+    case'View All Roles':
+        return viewAllRoles(queries.);
+    case'View Employees':
+        return viewEmployees(queries.);
+    case'Add Department':
+        return addDepartment(queries.);
+    case'Add Role':
+        return addRole(queries.);
+    case'Add Employee':
+        return addEmployee(queries.);
     case 'Exit':
         return closeApp;
+    }
+};
+
+// Query Functions
+
+const viewAllDepartments = () => {
+    connection.query(queries. ,(err, res) => {
+        if (err) {
+            throw new Error('Unable to view departments')
+        }
+        mainMenuHandler();
+    })
+    
 }
-}
+
+const viewAllRoles = () => {
+    connection.query(queries. ,(err, res) => {
+        if (err) {
+            throw new Error('Unable to access database')
+        }
+        mainMenuHandler();
+    }); 
+};
+
+const viewEmployees = () => {
+    connection.query(queries. ,(err, res) => {
+        if (err) {
+            throw new Error('Unable to view employees')
+        }
+        mainMenuHandler();
+    });
+    
+};
+
+const addDepartment = async () => {
+    connection.query(queries. ,(err, res) => {
+        if (err) {
+            throw new Error('Unable to add department')
+        }
+        mainMenuHandler();
+    });
+};
+
+const addRole = async () => {
+    connection.query(queries. ,(err, res) => {
+        if (err) {
+            throw new Error('Unable to add role')
+        }
+        mainMenuHandler();
+    });
+};
+
+const addEmployee = async () => {
+    connection.query(queries. ,(err, res) => {
+        if (err) {
+            throw new Error('Unable to add employee')
+        }
+
+    mainMenuHandler();
+    });
+};
+
+// Add additional functionality
+    
+
+
+
+
+
 // Close database connection
 const closeApp = function () {
     console.log('Thank you for using our app. Goodbye!')
