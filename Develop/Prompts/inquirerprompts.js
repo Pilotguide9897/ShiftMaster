64c464766f840c292cbf4a0a829254fb3312406a
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-04-06 11:12:41 -0300

    implementing and testing

--- Develop/Prompts/inquirerprompts.js
+++ Develop/Prompts/inquirerprompts.js
@@ -1,5 +1,5 @@
 const inquirer =require('inquirer');
-const { getDynamicChoices } = require('../index');
+// const { getDynamicChoices } = require('../index');
 const nameRegex = /^(?!.*  )[A-Za-z ]{3,30}$/;
 const numberRegex = /^\d{1,10}$/;
 
@@ -8,116 +8,116 @@ const userAction = [
         type: 'list',
         name: 'action',
         message: 'What would you like to do?',
-        choices:['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'View Employees by Manager', 'View Employees by Department', 'Delete Department', 'Delete Role', 'Delete Employee', 'Get Budget by Department', 'Quit']
+        choices:['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'View Employees by Manager', 'View Employees by Department', 'Delete Department', 'Delete Role', 'Delete Employee', 'Get Budget by Department', 'Exit']
     }
 ]
 
-const addDeptName = [
-    {
-        name: 'addDepartmentName',
-        type: 'input',
-        message: 'What is the name of the department?',
-        validate: (input) => {
-            if (!nameRegex.test(input)) {
-            return 'Please input a valid department title.'
-            }
-            return true;
-        }
-    }
-]
+// const addDeptName = [
+//     {
+//         name: 'addDepartmentName',
+//         type: 'input',
+//         message: 'What is the name of the department?',
+//         validate: (input) => {
+//             if (!nameRegex.test(input)) {
+//             return 'Please input a valid department title.'
+//             }
+//             return true;
+//         }
+//     }
+// ]
 
-const addRoles = [
-    {
-        type: 'input',
-        name: 'roleName',
-        message: 'What is the name of the role?',
-        validate: (input) => {
-            if (!nameRegex.test(input)) {
-            return 'Please input a role title.'
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        name: 'roleWage',
-        message: 'What is the salary of the role?',
-        validate: (input) => {
-            if (!numberRegex.test(input)) {
-            return 'Please input a valid numerical amount, exlucing hypens, dashes, or spaces.'
-            }
-            return true;
-        }
-    },
-    {
-        type: 'list',
-        name: 'roleDept',
-        message: 'Which department does this role belong to?',
-        choices: ['Sales', 'Engineering', 'Finance', 'Legal', 'service', 'Information and Technology', 'Public Relations', 'Research and Development', 'Corporate Management', 'Quality Assurance', 'Human Resources']
-    }
-]
+// const addRoles = [
+//     {
+//         type: 'input',
+//         name: 'roleName',
+//         message: 'What is the name of the role?',
+//         validate: (input) => {
+//             if (!nameRegex.test(input)) {
+//             return 'Please input a role title.'
+//             }
+//             return true;
+//         }
+//     },
+//     {
+//         type: 'input',
+//         name: 'roleWage',
+//         message: 'What is the salary of the role?',
+//         validate: (input) => {
+//             if (!numberRegex.test(input)) {
+//             return 'Please input a valid numerical amount, exlucing hypens, dashes, or spaces.'
+//             }
+//             return true;
+//         }
+//     },
+//     {
+//         type: 'list',
+//         name: 'roleDept',
+//         message: 'Which department does this role belong to?',
+//         choices: ['Sales', 'Engineering', 'Finance', 'Legal', 'service', 'Information and Technology', 'Public Relations', 'Research and Development', 'Corporate Management', 'Quality Assurance', 'Human Resources']
+//     }
+// ]
 
-const addEmployee = [
-    {
-        type: 'input',
-        name: 'employeeFName',
-        message: 'What is the employee\'s first name?',
-        validate: (input) => {
-            if (!nameRegex.test(input)){
-                return 'Please enter a valid first name.';
-            }
-            return true;
-        },
-    },
-    {
-        type: 'input',
-        name: 'employeeLName',
-        message: 'What is the employee\'s surname?',
-        validate: (input) => {
-            if (!nameRegex.test(input)){
-                return 'Please enter a valid surname name.';
-            }
-            return true;
-        },
-    },
-    {
-        type: 'list',
-        name: 'employeeRole',
-        message: 'What is the employee\'s role',
-        choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer', 'Customer Service']
-    },
-    {
-        type: 'list',
-        name: 'employeeManager',
-        message: 'Who is the employee\'s manager?',
-        Choices: ['None', 'John Doe', 'Mike Chan', 'Ashley Rodriguez', 'Kevin Tupik', 'Kunal Singh', 'Malie Brown']
-    }
-]
+// const addEmployee = [
+//     {
+//         type: 'input',
+//         name: 'employeeFName',
+//         message: 'What is the employee\'s first name?',
+//         validate: (input) => {
+//             if (!nameRegex.test(input)){
+//                 return 'Please enter a valid first name.';
+//             }
+//             return true;
+//         },
+//     },
+//     {
+//         type: 'input',
+//         name: 'employeeLName',
+//         message: 'What is the employee\'s surname?',
+//         validate: (input) => {
+//             if (!nameRegex.test(input)){
+//                 return 'Please enter a valid surname name.';
+//             }
+//             return true;
+//         },
+//     },
+//     {
+//         type: 'list',
+//         name: 'employeeRole',
+//         message: 'What is the employee\'s role',
+//         choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer', 'Customer Service']
+//     },
+//     {
+//         type: 'list',
+//         name: 'employeeManager',
+//         message: 'Who is the employee\'s manager?',
+//         Choices: ['None', 'John Doe', 'Mike Chan', 'Ashley Rodriguez', 'Kevin Tupik', 'Kunal Singh', 'Malie Brown']
+//     }
+// ]
 
-const updateEmployee = [ //come back to...
-    {
-        type: 'list',
-        name: 'updateEmpRole',
-        message: 'Which employee\'s role would you like to update?',
-        choices: currentEmployeeChoices
-    }
-]
+// const updateEmployee = [ //come back to...
+//     {
+//         type: 'list',
+//         name: 'updateEmpRole',
+//         message: 'Which employee\'s role would you like to update?',
+//         choices: currentEmployeeChoices
+//     }
+// ]
 
-const updateEmployeeManager = [
-    {
-        type: 'list',
-        name: 'employeeofMan',
-        message: 'Please select an employee.',
-        choices: currentEmployeeChoices,
-    },
-    {
-        type: 'list',
-        name: 'newManager',
-        message: 'Please select employee\'s new manager',
-        choices: currentEmployeeChoices,
-    }
+// const updateEmployeeManager = [
+//     {
+//         type: 'list',
+//         name: 'employeeofMan',
+//         message: 'Please select an employee.',
+//         choices: currentEmployeeChoices,
+//     },
+//     {
+//         type: 'list',
+//         name: 'newManager',
+//         message: 'Please select employee\'s new manager',
+//         choices: currentEmployeeChoices,
+//     }
 
-]
+// ]
 
 // const viewEmployeesByManager = [
 //     {
@@ -128,75 +128,75 @@ const updateEmployeeManager = [
 //     }
 // ]
 
-async function viewEmployeesByManagerPrompt() {
-    return {
-      type: 'list',
-      name: 'managerSelection',
-      message: 'Please select a manager.',
-      choices: await getDynamicChoices(),
-    };
-  }
+// async function viewEmployeesByManagerPrompt() {
+//     return {
+//       type: 'list',
+//       name: 'managerSelection',
+//       message: 'Please select a manager.',
+//       choices: await getDynamicChoices(),
+//     };
+//   }
   
 
-const viewEmployeesByDepartment = [
-    {
-        type: 'list',
-        name: 'departmentEmployees',
-        message: 'Please select a department',
-        choices: await getDynamicChoices(),
-    }
-]
+// const viewEmployeesByDepartment = [
+//     {
+//         type: 'list',
+//         name: 'departmentEmployees',
+//         message: 'Please select a department',
+//         choices: await getDynamicChoices(),
+//     }
+// ]
 
-const deleteDepartment = [
-    {
-        type: 'list',
-        name: 'removeDepartment',
-        message: 'Which department would you like to remove?',
-        choices: deleteDepartmentChoices,
-    }
-]
+// const deleteDepartment = [
+//     {
+//         type: 'list',
+//         name: 'removeDepartment',
+//         message: 'Which department would you like to remove?',
+//         choices: deleteDepartmentChoices,
+//     }
+// ]
 
-const deleteRole = [
-    {
-        type: 'list',
-        name: 'removeRole',
-        message: 'Which role would you like to remove?',
-        choices: removeRoleChoices,
-    }
-]
+// const deleteRole = [
+//     {
+//         type: 'list',
+//         name: 'removeRole',
+//         message: 'Which role would you like to remove?',
+//         choices: removeRoleChoices,
+//     }
+// ]
 
-const deleteEmployee = [
-    {
-        type: 'list',
-        name: 'removeEmployee',
-        message: 'Which employee would you like to remove?',
-        choices: currentEmployeeChoices,
-    }
+// const deleteEmployee = [
+//     {
+//         type: 'list',
+//         name: 'removeEmployee',
+//         message: 'Which employee would you like to remove?',
+//         choices: currentEmployeeChoices,
+//     }
     
-]
+// ]
 
-const getUtilizedDepartmentBudget = [
-    {
-        type: 'list',
-        name: 'departmentBudget',
-        message: 'Please select a department',
-        choices: currentDepartmentChoices,
-    }
-]
+// const getUtilizedDepartmentBudget = [
+//     {
+//         type: 'list',
+//         name: 'departmentBudget',
+//         message: 'Please select a department',
+//         choices: currentDepartmentChoices,
+//     }
+// ]
 
 module.exports = {
-    userAction,
-    addDeptName,
-    addRoles,
-    addEmployee,
-    updateEmployee,
-    updateEmployeeManager,
-    viewEmployeesByManager,
-    viewEmployeesByDepartment,
-    deleteDepartment,
-    deleteRole,
-    deleteEmployee,
-    getUtilizedDepartmentBudget,
-    viewEmployeesByManagerPrompt,
+     userAction
+//     addDeptName,
+//     addRoles,
+//     addEmployee,
+//     updateEmployee,
+//     updateEmployeeManager,
+//     viewEmployeesByManager,
+//     viewEmployeesByDepartment,
+//     deleteDepartment,
+//     deleteRole,
+//     deleteEmployee,
+//     getUtilizedDepartmentBudget,
+//     viewEmployeesByManagerPrompt,
 };
 
--- Develop/index.js
+++ Develop/index.js
@@ -7,7 +7,7 @@ const dbConnection = require('./db');
 const mainMenuHandler = async function () {
     let response = await inquirer.prompt(inquirerPrompts.userAction);
 
-switch (response) { // I can add the other bonus objectives here too.
+switch (response.action) { 
     case 'View All Departments':
         return viewDepartments();
     case 'View All Roles':
@@ -18,7 +18,7 @@ switch (response) { // I can add the other bonus objectives here too.
         return addDepartment();
     case 'Add Role':
         return addRole();
-    case 'Add Employee': //need to 
+    case 'Add Employee': 
         return addEmployee();
     case 'View Employees by Manager':
         return viewEmployeeByManager();
@@ -70,103 +70,105 @@ const viewEmployees = () => {
     });
 };
 
-const addDepartment = async () => {
-    let {deptName} = await inquirer.prompt(inquirerPrompts.addDeptName);
-    dbConnection.query(queries.addDepartment, [deptName], (err, res) => {
-        if (err) {
-            throw new Error('Unable to add department')
-        };
-        console.log( `Department '${name}' added successfully!`);
-        mainMenuHandler();
-    });
-};
+// const addDepartment = async () => {
+//     let {deptName} = await inquirer.prompt(inquirerPrompts.addDeptName);
+//     dbConnection.query(queries.addDepartment, [deptName], (err, res) => {
+//         if (err) {
+//             throw new Error('Unable to add department')
+//         };
+//         console.log( `Department '${name}' added successfully!`);
+//         mainMenuHandler();
+//     });
+// };
 
-const addRole = async () => {
-    let {roleName, roleWage, roleDept} = await inquirer.prompt(inquirerPrompts.addRoles);
-    dbConnection.query(queries.addRole, [roleName, roleWage, roleDept], (err, res) => {
-        if (err) {
-            throw new Error('Unable to add role')
-        };
-        console.log(`Role '${title} successfully added!`);
-        mainMenuHandler();
-    });
-};
+// const addRole = async () => {
+//     let {roleName, roleWage, roleDept} = await inquirer.prompt(inquirerPrompts.addRoles);
+//     dbConnection.query(queries.addRole, [roleName, roleWage, roleDept], (err, res) => {
+//         if (err) {
+//             throw new Error('Unable to add role')
+//         };
+//         console.log(`Role '${title} successfully added!`);
+//         mainMenuHandler();
+//     });
+// };
 
-const addEmployee = async () => {
-    let {employeeFName, employeeLName, employeeRole, employeeManager} = await inquirer.prompt(inquirerPrompts.addEmployee);
-    dbConnection.query(queries.addEmployee, [employeeFName, employeeLName, employeeRole, employeeManager], (err, res) => {
-        if (err) {
-            throw new Error('Unable to add employee')
-        };
-        console.log(`Employee '${employeeFName} ${employeeLName}' successfully added!`);
-    mainMenuHandler();
-    });
-};
+// const addEmployee = async () => {
+//     let {employeeFName, employeeLName, employeeRole, employeeManager} = await inquirer.prompt(inquirerPrompts.addEmployee);
+//     dbConnection.query(queries.addEmployee, [employeeFName, employeeLName, employeeRole, employeeManager], (err, res) => {
+//         if (err) {
+//             throw new Error('Unable to add employee')
+//         };
+//         console.log(`Employee '${employeeFName} ${employeeLName}' successfully added!`);
+//     mainMenuHandler();
+//     });
+// };
 
-const updateEmployeeRole = async () => {
-    let {employeeID, employeeRole} = await inquirer.prompt();
-}
+// const updateEmployeeRole = async () => {
+//     let {employeeID, employeeRole} = await inquirer.prompt();
+// }
 
-// Functions to add additional functionality
-const updateEmployeeManager = async () => {
-    let {} = await inquirer.prompt();
-}
+// // Functions to add additional functionality
+// const updateEmployeeManager = async () => {
+//     let {} = await inquirer.prompt();
+// }
 
-const viewEmployeeByManager = async () => {
-    let {} = await inquirer.prompt();
-}
+// const viewEmployeeByManager = async () => {
+//     let {} = await inquirer.prompt();
+// }
 
-const viewEmployeesByDepartment = async () => {
-    let {} = await inquirer.prompt();
-}
+// const viewEmployeesByDepartment = async () => {
+//     let {} = await inquirer.prompt();
+// }
 
-const deleteDepartment = async () => {
-    let {} = await inquirer.prompt();
-}
+// const deleteDepartment = async () => {
+//     let {} = await inquirer.prompt();
+// }
 
-const deleteRole = async () => {
-    let {} = await inquirer.prompt();
-}
+// const deleteRole = async () => {
+//     let {} = await inquirer.prompt();
+// }
 
-const deleteEmployee = async () => {
-    let {} = await inquirer.prompt();
-}
+// const deleteEmployee = async () => {
+//     let {} = await inquirer.prompt();
+// }
 
-const getBudgetByDepartment = async () => {
-    let {} = await inquirer.prompt();
-}
+// const getBudgetByDepartment = async () => {
+//     let {} = await inquirer.prompt();
+// }
 
 
-// Managing manager names for dynamic prompts.
-const currentManagerNames = async () => {
-    await new Promise ((resolve, reject) => {
-        dbConnection.query(queries.getManagerNames, (err, res) =>
-        {
-            if (err) {
-            console.log ('Unable to fetch manager names.');
-        } else {
-            resolve (res);
-        }
-        });
-    });
-};
+// // Managing manager names for dynamic prompts.
+// const currentManagerNames = async () => {
+//     await new Promise ((resolve, reject) => {
+//         dbConnection.query(queries.getManagerNames, (err, res) =>
+//         {
+//             if (err) {
+//             console.log ('Unable to fetch manager names.');
+//         } else {
+//             resolve (res);
+//         }
+//         });
+//     });
+// };
 
-const getDynamicChoices = async () => {
-    try {
-        const currentManagers = await currentManagerNames();
-        const managerNames = currentManagers.map(manager => `${manager["Manager Name"]} ${manager["Manager Surname"]}`);
-        return managerNames;
-    } catch (err) {
-        console.error(err);
-        return [];
-    }
-}
+// const getDynamicChoices = async () => {
+//     try {
+//         const currentManagers = await currentManagerNames();
+//         const managerNames = currentManagers.map(manager => `${manager["Manager Name"]} ${manager["Manager Surname"]}`);
+//         return managerNames;
+//     } catch (err) {
+//         console.error(err);
+//         return [];
+//     }
+// }
 
+// Initialize the application 
+mainMenuHandler();
 
 // Close database connection
 const closeApp = function () {
     console.log('Thank you for using our app. Goodbye!')
-    connection.end();
+    dbConnection.end();
 } 
 
-module.exports.getDynamicChoices = getDynamicChoices;
+// module.exports.getDynamicChoices = getDynamicChoices;
